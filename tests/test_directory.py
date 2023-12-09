a4c512549fb9df92f89c9c93efb70bd7ce9dbee2
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-09 18:14:35 +0100

    Update tests with limks and size

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -7,25 +7,39 @@ import configparser
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read("config/app.ini")
-BLOB_DIRECTORY = CONFIG["Blobs"]["blob_directory"]
 
 def test_directory_creation():
     """Test directory didn't exist and is created when instantiating a Blob Service."""
-    if os.path.exists(BLOB_DIRECTORY):
-        pytest.skip("Directory already exists")
 
-    BlobService(BLOB_DIRECTORY)
+    # Mock directories
+    test_blobs_directory = "tmp/blobs_directory"
+    test_links_directory = "tmp/links_directory"
 
-    assert os.path.exists(BLOB_DIRECTORY)
-    
+    if os.path.exists(test_blobs_directory) or os.path.exists(test_links_directory):
+        pytest.skip("Directories already exists")
+
+    BlobService(test_blobs_directory, test_links_directory, 0)
+
+    assert os.path.exists(test_blobs_directory) and os.path.exists(test_links_directory)
+
+    # Can remove the directories because they are empty, they were just created
+    os.rmdir(test_blobs_directory)
+    os.rmdir(test_links_directory)
+
 
 def test_directory_inst_altered():
     """Test if the directory doesn't change if it already exists.
     It should not change because it should not be created again nor add or remove files."""
-    if not os.path.exists(BLOB_DIRECTORY):
-        pytest.skip("Directory does not exist")
 
-    last_mod = os.path.getmtime(BLOB_DIRECTORY)
-    BlobService(BLOB_DIRECTORY)
+    blobs_directory = CONFIG["Blobs"]["blobs_directory"]
+    links_directory = CONFIG["Blobs"]["links_directory"]
 
-    assert last_mod == os.path.getmtime(BLOB_DIRECTORY)
+    if not os.path.exists(blobs_directory) and not os.path.exists(links_directory):
+        pytest.skip("Directories do not exist")
+
+    blob_last_mod = os.path.getmtime(blobs_directory)
+    link_last_mod = os.path.getmtime(blobs_directory)
+    BlobService(blobs_directory, links_directory, 0)
+
+    assert blob_last_mod == os.path.getmtime(blobs_directory) and link_last_mod == os.path.getmtime(links_directory)
+
