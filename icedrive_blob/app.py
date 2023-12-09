8a11a310956b48e1fe0ab6a082c131b9cc76c770
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-12-09 11:56:05 +0100

    Update server app with defaults configs

--- icedrive_blob/app.py
+++ icedrive_blob/app.py
@@ -1,7 +1,9 @@
 """Authentication service application."""
 
+import configparser
 import logging
 import sys
+import os.path
 from typing import List
 
 import Ice
@@ -17,8 +19,16 @@ class BlobApp(Ice.Application):
         adapter = self.communicator().createObjectAdapter("BlobAdapter")
         adapter.activate()
 
-        servant = BlobService()
-        servant_proxy = adapter.addWithUUID(servant)
+        config = configparser.ConfigParser()
+
+        # Load the configuration file, default to config/app.ini
+        path = args[2] if len(args) > 2 else os.path.join(os.path.dirname(__file__), "..", "config", "app.ini")
+        config.read(path)
+        logging.debug("Configuration: %s file loaded.", path)
+
+        servant = BlobService(config["Blobs"]["blob_directory"])
+
+        servant_proxy = adapter.addWithUUID(servant) if config["Server"]["random_proxy"] != "false" else adapter.add(servant, self.communicator().stringToIdentity("BlobService"))
 
         logging.info("Proxy: %s", servant_proxy)
 
@@ -30,5 +40,8 @@ class BlobApp(Ice.Application):
 
 def main():
     """Handle the icedrive-authentication program."""
+    # usage: %prog [ice_config_file] [service_config_file]
+    # default ice_config_file: config/blob.config
+    ice_config_file = sys.args[1] if len(sys.args) > 1 else os.path.join(os.path.dirname(__file__), "..", "config", "blob.config")
     app = BlobApp()
-    return app.main(sys.argv)
+    return app.main(sys.argv, ice_config_file)
